25a924a54b79ec89d451496c9f71017c395ed9eb
nicolaedragu <118878481+nicolaedragu@users.noreply.github.com>
2023-04-26 21:51:03 +0300

    Update quadtree.c

--- checker/quadtree.c
+++ checker/quadtree.c
@@ -2,9 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct QuadTree
+{
+    unsigned char culoare1;
+} QuadTree;
+
 int main()
 {
     
-    
-    
 }
+
+/*
+https://curs.upb.ro/2022/pluginfile.php/509159/mod_assign/intro/SDA___Tema_2_2023.pdf
+https://thoughtbot.com/blog/how-to-handle-large-amounts-of-data-on-maps
+*/
